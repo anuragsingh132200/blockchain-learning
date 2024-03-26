ad44a6863ef2b3ace1abb37b0dd338f92a7b1706
Anurag singh <144618219+anuragsingh132200@users.noreply.github.com>
2024-03-26 19:52:37 +0530

    c: Removal of Chain-Link Integration

--- FundMe.sol
+++ FundMe.sol
@@ -1,18 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.8;
 
-import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
-import "./PriceConverter.sol";
+// Remove the import statement for PriceConverter.sol
 
 error NotOwner();
 
 contract FundMe {
-    using PriceConverter for uint256;
+    // Remove the "using PriceConverter for uint256;" statement
 
     mapping(address => uint256) public addressToAmountFunded;
     address[] public funders;
 
-    // Could we make this constant?  /* hint: no! We should make it immutable! */
     address public /* immutable */ i_owner;
     uint256 public constant MINIMUM_USD = 50 * 10 ** 18;
     
@@ -21,9 +19,8 @@ contract FundMe {
     }
 
     function fund() public payable {
-        require(msg.value.getConversionRate() >= MINIMUM_USD, "You need to spend more ETH!");
-    
-        // Check if the sender address already exists in the funders array
+        require(msg.value >= MINIMUM_USD, "You need to spend more ETH!");
+        
         bool isFunder = false;
         for(uint256 i = 0; i < funders.length; i++) {
             if(funders[i] == msg.sender) {
@@ -31,21 +28,13 @@ contract FundMe {
                 break;
             }
         }
-        // If sender address is not already in the funders array, add it
         if(!isFunder) {
             addressToAmountFunded[msg.sender] += msg.value;
             funders.push(msg.sender);
         }
     }
     
-    function getVersion() public view returns (uint256){
-        // ETH/USD price feed address of Sepolia Network.
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
-        return priceFeed.version();
-    }
-    
     modifier onlyOwner {
-        // require(msg.sender == owner);
         if (msg.sender != i_owner) revert NotOwner();
         _;
     }
@@ -55,30 +44,10 @@ contract FundMe {
             address funder = funders[funderIndex];
             addressToAmountFunded[funder] = 0;
         }
-        funders = new address[](0);
-        // // transfer
-        // payable(msg.sender).transfer(address(this).balance);
-        // // send
-        // bool sendSuccess = payable(msg.sender).send(address(this).balance);
-        // require(sendSuccess, "Send failed");
-        // call
+        funders = new address ;
+        
         (bool callSuccess, ) = payable(msg.sender).call{value: address(this).balance}("");
         require(callSuccess, "Call failed");
-    }
-    // Explainer from: https://solidity-by-example.org/fallback/
-    // Ether is sent to contract
-    //      is msg.data empty?
-    //          /   \ 
-    //         yes  no
-    //         /     \
-    //    receive()?  fallback() 
-    //     /   \ 
-    //   yes   no
-    //  /        \
-    //receive()  fallback()
-    function transferOwnership(address newOwner) public onlyOwner {
-        require(newOwner != address(0), "Invalid new owner address");
-        i_owner = newOwner;
     }
 
     fallback() external payable {
@@ -88,16 +57,4 @@ contract FundMe {
     receive() external payable {
         fund();
     }
-
 }
-
-// Concepts we didn't cover yet (will cover in later sections)
-// 1. Enum
-// 2. Events
-// 3. Try / Catch
-// 4. Function Selector
-// 5. abi.encode / decode
-// 6. Hash with keccak256
-// 7. Yul / Assembly
-
-
